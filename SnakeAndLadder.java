91388ca72880f7010c18d616ad1bbd798a15b830
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 12:34:51 +0530

    ADDED :random to get dice value

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,23 @@
 package com.program.snakeandladder;
+
+import java.util.Random;
+class SnakeAndLadderGame{
+	public static void random() {
+		Random random =new Random();
+		int rollDie=random.nextInt(6)+1;
+	}
+}
 //MainClass
 public class SnakeAndLadder {
  //mainMethod
-        public static void main(String[] args) {
-                  int playerA=0;
-                  int rollDie;
-                  int players;
-        }
+	public static void main(String[] args) {
+		
+		//initializing variable playerAPosition and rollDie
+		int playerAPosition=0;
+		int player=1;
+		SnakeAndLadderGame.random();
+		
+				
+	}	
 }
+	
