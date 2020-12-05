f2d3e218852e9771afebf8161d4d7ad9de3b3452
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 12:27:43 +0530

    ADDED :welcome message

---
+++ SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.program.snakeandladder;
+//MainClass
+public class SnakeAndLadder {
+ //mainMethod
+	public static void main(String[] args) {
+		
+		System.out.print("WELCOME TO SNAKE AND LADDER GAME");
+	}	
+}
