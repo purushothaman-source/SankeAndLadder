4e29a864c1248c76fd3af278339905c7873c2755
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 13:49:17 +0530

    ADDED :used ladder and snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,9 +2,29 @@ package com.program.snakeandladder;
 
 import java.util.Random;
 class SnakeAndLadderGame{
-	public static void random() {
+	
+
+	
+	public static void random(int playerAPosition) {
 		Random random =new Random();
 		int rollDie=random.nextInt(6)+1;
+		System.out.println(logic(playerAPosition));
+	}
+	public static int logic(int playerAPosition) {
+		Random random =new Random();
+		int choice=random.nextInt(3);
+		switch(choice) {
+		case 1:playerAPosition=playerAPosition;
+			   break;
+		case 2:playerAPosition=playerAPosition+7;
+			 break;
+		case 3:if ((playerAPosition-7)>=0)
+			playerAPosition=playerAPosition-7;
+			break;
+		
+		
+		}
+	return playerAPosition;	
 	}
 }
 //MainClass
@@ -15,9 +35,9 @@ public class SnakeAndLadder {
 		//initializing variable playerAPosition and rollDie
 		int playerAPosition=0;
 		int player=1;
-		SnakeAndLadderGame.random();
+		SnakeAndLadderGame.random(playerAPosition);
 		
 				
 	}	
 }
-	
+
