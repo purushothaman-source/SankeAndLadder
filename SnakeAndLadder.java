598834b2c180149837c4a6cf436934b8c83622d5
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 12:30:27 +0530

    ADDED : initilized first player position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,8 +2,9 @@ package com.program.snakeandladder;
 //MainClass
 public class SnakeAndLadder {
  //mainMethod
-	public static void main(String[] args) {
-		
-		System.out.print("WELCOME TO SNAKE AND LADDER GAME");
-	}	
+        public static void main(String[] args) {
+                  int playerA=0;
+                  int rollDie;
+                  int players;
+        }
 }
